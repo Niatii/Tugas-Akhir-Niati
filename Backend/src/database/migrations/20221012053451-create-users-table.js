df4bb79cef66af87d8de4d52ab0c35d3fca0191b
Niati <niatiniati545@gmail.com>
2026-02-23 09:52:14 +0700

    first commit

---
+++ Backend/src/database/migrations/20221012053451-create-users-table.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ Backend/src/features/user/validations/request/update-user.request.ts
@@ -0,0 +1,13 @@
+import * as Joi from "joi";
+import { getUserRoleEnums } from "../../enums/user-role.enum";
+
+const userRoleEnum = getUserRoleEnums().map((value) => +value.id);
+
+export const updateUserSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  username: Joi.string().optional().allow(null, ""),
+  role: Joi.number()
+    .optional()
+    .valid(...userRoleEnum),
+}).options({ abortEarly: false });
